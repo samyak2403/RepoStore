01d0cd9e0dc74587c972fb243a51210228706111
Samyak Kamble <samyakkamble6659@gmail.com>
2026-01-18 17:26:16 +0530

    Fix OAuth scope: remove public_repo write permission, only request read:user

--- app/src/main/java/com/samyak/repostore/data/auth/GitHubAuth.kt
+++ app/src/main/java/com/samyak/repostore/data/auth/GitHubAuth.kt
@@ -37,7 +37,7 @@ object GitHubAuth {
     private const val ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token"
     private const val USER_API_URL = "https://api.github.com/user"
     
-    private const val SCOPE = "public_repo read:user"
+    private const val SCOPE = "read:user"
     private const val MAX_POLL_ATTEMPTS = 60 // 5 minutes max
 
     private val client = OkHttpClient()
